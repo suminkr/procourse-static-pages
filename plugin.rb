b77c520f9c3fc02c605a375c5b28b9eb76d1ca6d
suminkr <138836255+suminkr@users.noreply.github.com>
2025-02-04 02:43:16 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: procourse-static-pages
-# about: Adds the ability to create static pages for Discourse.
+# about: Adds the ability to create static pages for Discourse and redirects root to /page/home/10
 # version: 0.1
 # author: Joe Buhlig joebuhlig.com
 # url: https://www.github.com/procourse/procourse-static-pages
@@ -13,20 +13,12 @@ register_asset "stylesheets/pc-static-pages.scss"
 load File.expand_path('../lib/pc_static_pages/engine.rb', __FILE__)
 
 after_initialize do
-  # 루트 페이지 리디렉션을 위한 컨트롤러 정의
-  require_dependency 'application_controller'
-  class ::CustomHomepageController < ::ApplicationController
-    def index
-      redirect_to "/page/home/10/"
-    end
-  end
-
-  # 기존 라우트보다 우선순위를 높이도록 prepend를 이용하여 루트 라우트 재정의
+  # 루트 경로 (/) 접근 시 /page/home/10으로 리디렉션합니다.
   Discourse::Application.routes.prepend do
-    get "/" => "custom_homepage#index"
+    get "/" => redirect("/page/home/10")
   end
 
-  # 기존 procourse-static-pages 플러그인 라우트
+  # 기존 procourse-static-pages 플러그인 라우트 설정
   Discourse::Application.routes.append do
     get '/admin/plugins/procourse-static-pages' => 'admin/plugins#index', constraints: StaffConstraint.new
     get '/page/:slug/:id' => 'pc_static_pages/pages#show'
