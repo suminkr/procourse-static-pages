be5816095e7d796f31f9974b5e06ba99704618bd
suminkr <138836255+suminkr@users.noreply.github.com>
2025-02-04 03:03:14 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,6 @@
 # name: procourse-static-pages
-# about: Adds the ability to create static pages for Discourse and renders the content of /page/home/10 directly on /
+# about: Adds the ability to create static pages for Discourse and renders the content of /page/home/10 directly on /,
+#        and updates the main logo link to point to /
 # version: 0.1
 # author: Joe Buhlig joebuhlig.com
 # url: https://www.github.com/procourse/procourse-static-pages
@@ -9,18 +10,18 @@ enabled_site_setting :procourse_static_pages_enabled
 add_admin_route 'procourse_static_pages.title', 'procourse-static-pages'
 
 register_asset "stylesheets/pc-static-pages.scss"
-register_asset "javascripts/update-logo-link.js"  # 메인로고 링크 변경용 스크립트
+register_asset "javascripts/update-logo-link.js"
 
 load File.expand_path('../lib/pc_static_pages/engine.rb', __FILE__)
 
 after_initialize do
-  # 루트 URL (/) 접근 시 pc_static_pages/pages#show 액션을 호출하여
-  # 기본 파라미터 { slug: "home", id: "10" }를 전달하고 직접 렌더링합니다.
+  # 루트 URL (/)에 접근하면 pc_static_pages/pages#show 액션이 기본 파라미터와 함께 호출되어
+  # /page/home/10의 내용이 직접 렌더링됩니다.
   Discourse::Application.routes.prepend do
     get "/" => "pc_static_pages/pages#show", defaults: { slug: "home", id: "10" }
   end
 
-  # 기존 procourse-static-pages 플러그인 라우트 설정 유지
+  # 기존 procourse-static-pages 플러그인의 기타 라우트 설정
   Discourse::Application.routes.append do
     get '/admin/plugins/procourse-static-pages' => 'admin/plugins#index', constraints: StaffConstraint.new
     get '/page/:slug/:id' => 'pc_static_pages/pages#show'
